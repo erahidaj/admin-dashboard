d82227db79be104a8de592c84be49bcaa474a779
Era Hidaj <erahidaj@gmail.com>
2026-06-10 11:19:12 +0200

    build: use standard Vite dist output

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
-        outDir: 'build',
+        outDir: 'dist',
     },
     server: {
         port: 3000,
